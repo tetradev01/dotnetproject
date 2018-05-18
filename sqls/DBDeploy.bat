7c9cc5a382f18ef809ebd30d8fa77e3fb1295aaf
TetraDev01 <tetradev01@tetratutorials.com>
2018-05-18 21:46:16 +0530

    .bat file updated.

--- sqls/DBDeploy.bat
+++ sqls/DBDeploy.bat
@@ -3,13 +3,11 @@
 SET DIR=%~d0%~p0%
 
 SET database.name="customers"
-SET sql.files.directory="%DIR%"
+SET sql.files.directory="%DIR%\sqls"
 SET server.database="USER-PC\SQLEXPRESS"
 SET repository.path="https://github.com/chucknorris/roundhouse.git"
 SET version.file="_BuildInfo.xml"
 SET version.xpath="//buildInfo/version"
 SET environment="LOCAL"
 
-"%DIR%rh.exe" /d=%database.name% /f=%sql.files.directory% /s=%server.database% /vf=%version.file% /vx=%version.xpath% /r=%repository.path% /env=%environment% /simple
-
-pause
+"%DIR%rh" --silent /d=%database.name% /f=%sql.files.directory% /s=%server.database%  /env=%environment%
