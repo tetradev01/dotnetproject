e4354176542c4b346eae777e3c5d41fd06c54021
TetraDev01 <tetradev01@tetratutorials.com>
2018-05-18 22:43:58 +0530

    sql changed.

---
+++ sqls/up/0003_InsertData.sql
@@ -0,0 +1,11 @@
+INSERT INTO customer_data (firstname, lastname, phone, email)
+VALUES ('Cardinal', 'Tom B. Erichsen', '234234234', 't@tetra.com');
+
+INSERT INTO customer_data (firstname, lastname, phone, email)
+VALUES ('Wolski', 'Zbyszek', '4645646', 't@tetra.com');
+
+INSERT INTO customer_data (firstname, lastname, phone, email)
+VALUES ('Wilman Kala', 'Matti Karttunen', '346456456', 't@tetra.com');
+
+INSERT INTO customer_data (firstname, lastname, phone, email)
+VALUES ('White Clover Markets', 'Karl Jablonski	', '678678', 'e@tetra.com');
